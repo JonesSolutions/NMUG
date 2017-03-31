0f095653b3fdd00e4b23d5b2121e6025f1079ae6
klowey <klowey@W766299>
2017-03-30 21:09:47 -0600

    Added models for meeting and jobs

---
+++ NMUG/Models/Jobs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NMUG.Models
+{
+    public class Jobs
+    {
+        public int JobId { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Posting Date")]
+        public DateTime JobPostDate { get; set; }
+
+
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "Description")]
+        public string JobDescription { get; set; }       
+
+
+    }
+}
---
+++ NMUG/Models/Meeting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+            
+
+namespace NMUG.Models
+{
+    public class Meeting
+    {
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Display(Name = "Meeting Date")]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)] 
+        public DateTime MeetingDate { get; set; }
+        
+        [Display(Name = "Meeting Time")]
+        public DateTime MeetingTime { get; set; }
+
+        [RegularExpression(@"^[\d|A-Z]+[a-zA-Z''-'\s]*$")]
+        [Display(Name = "Meeting Location")]
+        public string MeetingLocation { get; set; }
+        
+        [Display(Name = "Meeting Presenter")]
+        public string MeetingPresenter { get; set; }
+
+       
+        [Display(Name = "Meeting Description")]
+        [DataType(DataType.MultilineText)]
+        [StringLength(5000)]
+        public string MeetingDescription { get; set; }
+
+
+    }
+}
