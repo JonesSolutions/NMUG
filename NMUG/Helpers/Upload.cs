e6b4b28d4c27785fd944d7746d307831b09c715c
Brian <Brian@JOnessolutions.com>
2017-06-10 15:09:29 -0600

    aded helper

---
+++ NMUG/Helpers/Upload.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NMUG.Models;
+
+namespace NMUG.Helpers
+{
+    public class Upload
+    {
+
+        //private static IHostingEnvironment _environment;
+
+        internal static async Task UploadFile(Models.Jobs jobs, ICollection<IFormFile> files, IHostingEnvironment environment)
+        {
+
+
+            var uploads = Path.Combine(environment.ContentRootPath, "uploads");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                        jobs.FileName = file.FileName;
+                    }
+
+                }
+            }
+        }
+
+
+    }
+}
